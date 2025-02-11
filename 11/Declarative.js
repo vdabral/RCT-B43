75ed874085101a0d38e25db95a3d83d8f745f839
vdabral <vdabral21007@gmail.com>
2025-02-11 23:21:35 +0530

    first commit

---
+++ 11/Declarative.js
@@ -0,0 +1,8 @@
+let head = React.createElement(
+  "h1",
+  { id: "declarative" },
+  "Hello World! : Declarative"
+);
+let root = ReactDOM.createRoot(
+  document.querySelector("#root-declarative")
+).render(head);
---
+++ 11/Imperative.js
@@ -0,0 +1,4 @@
+let head = document.createElement("h1");
+head.innerText = "Hello, World!: Imperative";
+head.setAttribute("class", "imperative");
+document.querySelector("#root-imperative").append(head);
