4196cc42606c6f1842851bb695867bb5c1fb14b2
vdabral <138614103+vdabral@users.noreply.github.com>
2025-02-11 23:23:20 +0530

    Update Imperative.js

--- 11/Imperative.js
+++ 11/Imperative.js
@@ -1,4 +1,4 @@
-let head = document.createElement("h1");
+let head = document.createElement("p");
 head.innerText = "Hello, World!: Imperative";
 head.setAttribute("class", "imperative");
 document.querySelector("#root-imperative").append(head);
